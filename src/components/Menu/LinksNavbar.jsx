0ac104aeba14043ab1242133a24fe6ad2ce9878d
danielablancom <blancodanielac@gmail.com>
2020-09-22 19:13:37 -0300

    Router added in file App.js with link to test action, components of menu added along with their respective components

---
+++ src/components/Menu/LinksNavbar.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react'; 
+
+function LinksNavbar () {
+
+    return (
+        <Fragment>
+        <div>
+           <ul>
+               <li>Conócenos</li>
+               <li>Que Hacemos</li>
+                <li> Hazte Parte </li>
+               <i class="material-icons">favorite</i>
+               <li>Te Contamos</li>
+               <li> Contáctanos</li>
+
+           </ul> 
+        </div>
+
+
+            
+        </Fragment>
+    ) };
+
+export default LinksNavbar;
