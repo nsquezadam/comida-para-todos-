2cbacb7f2d24723ef2eb55369aba331caa200703
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-09-23 12:52:05 -0300

    cambio css

--- src/components/Menu/Logo.jsx
+++ src/components/Menu/Logo.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import logoCPT from "../../images/logoCPT.png";
 
+
 const LogoHome = () => {
   return (
     <Fragment>
