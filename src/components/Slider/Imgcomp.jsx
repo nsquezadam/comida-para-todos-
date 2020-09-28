d3e04cd18b314285b30d3407f0b723b5b6be775a
danielablancom <blancodanielac@gmail.com>
2020-09-28 08:23:33 -0300

    added text to img slider

--- src/components/Slider/Imgcomp.jsx
+++ src/components/Slider/Imgcomp.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { SliderText } from '../SliderText/SliderText';
+
 
 function ImgComp({ src }) {
     let imgStyles ={
         width:100+"%",
         height:"auto"
     }
-    return <img src={src} alt="slider-Img" style={imgStyles}></img>;
+    
+    return (
+      <div>
+        <img src={src} alt="slider-Img" style={imgStyles}></img>;
+        <SliderText />
+        
+      </div>
+    );
 }
 
 export default ImgComp;
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React,{useState}  from 'react';
+import React,{useState, useEffect}  from 'react';
 import ImgComp from './Imgcomp'
 import i1 from "./Pics/1.jpg";
 import i2 from "./Pics/2.jpg";
@@ -9,36 +9,49 @@ import chevronLeft from '@iconify/icons-bytesize/chevron-left';
 import chevronRight from '@iconify/icons-bytesize/chevron-right';
 
 
-
-
-
 function Slider() {
-    const [X, SetX] = useState(0)
-    //let sliderArr = [1, 2, 3];
-    let sliderArr = [<ImgComp src={i1}/>, <ImgComp src={i2}/>, <ImgComp src={i3}/>];
-    
+  const [X, SetX] = useState(0);
+  //let sliderArr = [1, 2, 3];
 
-    const goLeft =() => {
-        X === 0? SetX(-100*(sliderArr.length -1)) : SetX (X + 100);
-    }
-    const goRight = () => {
-        (X === -100*(sliderArr.length -1)) ? SetX(0) : SetX (X - 100);
-    }
 
-    return (
-        <div className='slider'>
-            {sliderArr.map((item, index) => {
-                return (
-                    <div key={index} className='slide' style={ {transform:`translateX(${X}%)`} }>
-                        {item}
-                    </div>
-                )
-            })}
 
-            <button id="goLeft" onClick={goLeft}><Icon icon={chevronLeft} color="#FFFFFF" width="3vh" /></button>
-            <button id="goRight" onClick={goRight}><Icon icon={chevronRight} color="#FFFFFF" width="3vh" /></button>
+  let sliderArr = [
+    <ImgComp src={i1} />,
+    <ImgComp src={i2} />,
+    <ImgComp src={i3} />,
+   
+  ];
+
+  const goLeft = () => {
+    X === 0 ? SetX(-100 * (sliderArr.length - 1)) : SetX(X + 100);
+  };
+  const goRight = () => {
+    X === -100 * (sliderArr.length - 1) ? SetX(0) : SetX(X - 100);
+  };
+
+  return (
+    <div className="slider">
+      {sliderArr.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="slide"
+            style={{ transform: `translateX(${X}%)` }}
+          >
+            {item}
+          </div>
+        );
+      })}
+      
+      <button id="goLeft" onClick={goLeft}>
+        <Icon icon={chevronLeft} color="#FFFFFF" width="3vh" />
+      </button>
+      <button id="goRight" onClick={goRight}>
+        <Icon icon={chevronRight} color="#FFFFFF" width="3vh" />
+      </button>
     </div>
-
-    )
+    
+  );
 }
-export default Slider
+
+export default Slider;
