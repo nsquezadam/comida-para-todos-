88bc37934ecfaba7a38f19e5a54db62cb5fea422
AlgrisMarquez <algrismarquez16@gmail.com>
2020-09-28 09:00:04 -0300

    nueva ramapara trabajo personal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/Menu/Navbar';
 import LinksRRSS from './components/Menu/LinksRRSS';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import '../src/Styles/Slider.scss'
-import Slider from './components/slider/Slider';
+import Slider from './components/Slider/Slider';
 
 function App() {
   return (
