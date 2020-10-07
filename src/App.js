99e25e56781c10543582d8f3f6043947a52dc148
Natalia Quezada <66387884+nsquezadam@users.noreply.github.com>
2020-10-07 11:30:54 -0300

    modificacion cod slider

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { Fragment } from 'react';
 import '../src/assets/styles/App.scss';
 //import LinksRRSS from './components/Menu/LinksRRSS';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-//import Navbar from './components/Menu/Navbar'
+import NavBarR from './components/Menu/toolbar/NavbarR'
 import Slider from './components/slider/Slider';
-// import Message from './components/Message/Message';
-// import Detail from '../src/components/Detail/Detail';
-// import Goals from './components/Goals/Goals';
-// import BecomePart from './components/Community/BecomePart'
-//import News from './components/Community/News';
-// import ContacForm from './components/contacForm/ContacForm';
-// import Testimonials from './components/testimonials/Testimonials';
- import NavBarR from './components/Menu/toolbar/NavbarR'
+import Message from './components/Message/Message';
+import Detail from '../src/components/Detail/Detail';
+import Goals from './components/Goals/Goals';
+import BecomePart from './components/Community/BecomePart'
+import News from './components/Community/News';
+import ContacForm from './components/contacForm/ContacForm';
+import Testimonials from './components/testimonials/Testimonials';
+
 
 
 
@@ -27,9 +27,7 @@ function App() {
           <Router>
             <Switch>
               <Route path="/" exact>
-                {/* <Navbar /> */}
                 <NavBarR />
-               
 
               </Route>
               <Route path="/test">
@@ -43,30 +41,26 @@ function App() {
   
         </div>
         <div>
-          {/* <Message /> */}
-        </div>
-
-        <div>
-          {/* <Detail /> */}
+          <Message />
         </div>
         <div>
-           {/* <Goals/>  */}
+          <Detail />
         </div>
-       <div>
-       {/* <BecomePart /> */}
-       </div>
-       <div>
-         {/* <News /> */}
-       </div>
         <div>
-      {/* <Testimonials /> */}
-       </div>
-        <div>
-          {/* <ContacForm/> */}
+          <Goals/>
         </div>
-
-
-
+      <div>
+          <BecomePart /> 
+      </div>
+      <div>
+          <News /> 
+      </div>
+      <div>
+          <Testimonials />
+      </div>
+      <div>
+           <ContacForm/>
+      </div>
       </div>
     </Fragment>
   );
