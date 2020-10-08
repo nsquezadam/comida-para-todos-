854a4c336bf45faa5e126c6a42b872b5fb432990
AlgrisMarquez <algrismarquez16@gmail.com>
2020-10-08 10:04:41 -0300

    cambios y adaptar el home

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import NavBarR from './components/Menu/toolbar/NavbarR'
 //import Home from './components/Home'; 
 //import Navbar from './components/Menu/Navbar'
-<<<<<<< HEAD
-=======
-
->>>>>>> 529122975109552193bd11caf1e1397b4f022e08
 import Slider from './components/Slider/Slider';
 import Message from './components/Message/Message';
 import Detail from '../src/components/Detail/Detail';
@@ -19,11 +15,7 @@ import ContacForm from './components/contacForm/ContacForm';
 import Testimonials from './components/testimonials/Testimonials';
 import ActionLines from './components/Community/ActionLines'
 import SliderPart from './components/sliderPartners/SliderPart';
-<<<<<<< HEAD
-//import Footer from './components/footer/Footer';
-=======
 import Footer from './components/footer/Footer';
->>>>>>> 529122975109552193bd11caf1e1397b4f022e08
 
 function App() {
   return (
@@ -71,18 +63,6 @@ function App() {
         </div>
         <div>
           <Testimonials />
-<<<<<<< HEAD
-        </div>
-        <div>
-          <SliderPart />
-        </div>
-        <div>
-          <ContacForm />
-        </div>
-        <div>
-          {/* <Footer/> */}
-        </div>
-=======
       </div>
       <div>
         <SliderPart />
@@ -93,7 +73,6 @@ function App() {
       <div>
         <Footer style= {{height: '492px', background: '#333333'}}/> 
       </div>
->>>>>>> 529122975109552193bd11caf1e1397b4f022e08
       </div>
     </Fragment>
   );
--- src/components/Menu/Countries.jsx
+++ src/components/Menu/Countries.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
 import React from 'react'
 //$ npm install @material-ui/core
->>>>>>> 529122975109552193bd11caf1e1397b4f022e08
 //npm install @material-ui/icons
 //npm isntall @material-ui/core
 import LanguageIcon from '@material-ui/icons/Language';
