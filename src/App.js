5796b8d7df0863de339a7d5017b60c71267addae
danielablancom <blancodanielac@gmail.com>
2020-10-08 23:54:17 -0300

    Path and router changed

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import '../src/assets/styles/App.scss';
 //import LinksRRSS from './components/Menu/LinksRRSS';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-import NavBarR from './components/Menu/toolbar/NavbarR'
+// import NavBarR from './components/Menu/toolbar/NavbarR'; 
 
 
 //import Home from './components/Home'; 
 
 //import Navbar from './components/Menu/Navbar'
 
-import Slider from './components/Slider/Slider';
-import Message from './components/Message/Message';
- import Detail from '../src/components/Detail/Detail';
- import Goals from './components/Goals/Goals';
- import BecomePart from './components/Community/BecomePart'
- import ActionLines from './components/Community/ActionLines'
- import News from './components/Community/News';
-import ContacForm from './components/contacForm/ContacForm';
-import Testimonials from './components/testimonials/Testimonials';
-import SliderPart from './components/sliderPartners/SliderPart';
-import Footer from './components/footer/Footer';
+// import Slider from './components/Slider/Slider';
+// import Message from './components/Message/Message';
+//  import Detail from '../src/components/Detail/Detail';
+//  import Goals from './components/Goals/Goals';
+//  import BecomePart from './components/Community/BecomePart'
+//  import ActionLines from './components/Community/ActionLines'
+//  import News from './components/Community/News';
+// import ContacForm from './components/contacForm/ContacForm';
+// import Testimonials from './components/testimonials/Testimonials';
+// import SliderPart from './components/sliderPartners/SliderPart';
+// import Footer from './components/footer/Footer';
+import OurTeam from './components/OurTeam/OurTeam'; 
+import Home from './components/Home'; 
 
 
 
@@ -33,55 +35,18 @@ function App() {
 
   return (
     <Fragment>
-      <div  style={{height:'100%'}}>
-        <div>
+     
           <Router>
             <Switch>
               <Route path="/" exact>
-                <NavBarR />
-
+                <Home />
               </Route>
-              <Route path="/test">
-                {/* <LinksRRSS /> */}
-              </Route>
+              <Route path="/ conocenos">
+                <OurTeam />
+                </Route>
             </Switch>
           </Router>
-        </div>
-        <div>
-            <Slider autoPlay={5}/> 
-  
-        </div>
-        <div>
-           <Message /> 
-        </div>
-        <div>
-           <Detail /> 
-        </div>
-        <div>
-           <Goals/> 
-        </div>
-      <div>
-          <BecomePart /> 
-      </div>
-      <div>
-       <ActionLines/>
-      </div>
-      <div style={ {height:'1074px',background: '#F2F2F2'}}>
-           <News />  
-      </div>
-      <div>
-          <Testimonials />
-      </div>
-      <div>
-        <SliderPart />
-      </div>
-      <div>
-           <ContacForm/>
-      </div>
-      <div>
-        <Footer style= {{height: '492px', background: '#333333'}}/> 
-      </div>
-      </div>
+      
     </Fragment>
   );
 }
