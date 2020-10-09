9a75c68162c7cbb036cbd5c96beff1abf47451cc
danielablancom <blancodanielac@gmail.com>
2020-10-08 23:55:58 -0300

    creating routing from ourTeam component

---
+++ src/components/OurTeam/OurTeam.jsx
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import '../../assets/styles/components/OurTeam.scss'; 
+import ourteam from '../../assets/static/Pics/ourteam.jpg'
+
+const OurTeam = () => {
+    return (
+        <div className= "principal-container-ourTeam">
+            <div className="container-img-ourTeam">
+  <img className="principal-img-ourTeam" src={ourteam} alt={"ourteam"}></img>
+            </div>
+            <div className="linksToHome">
+
+            </div>
+            <div className="principal-text-ourTeam">
+
+            </div>
+            <div className="icon-gift">
+
+            </div>
+
+        </div>
+    ); 
+}
+
+export default OurTeam; 
