13f098f101fa8b7123f08c01f8fda062fdd575e1
danielablancom <blancodanielac@gmail.com>
2020-10-11 12:36:36 -0300

    images team members

--- src/components/OurTeam/TeamMembers.jsx
+++ src/components/OurTeam/TeamMembers.jsx
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import '../../assets/styles/components/TeamMembers.scss'; 
-
+import rafarincon from '../../assets/static/Pics/rafarincon.jpg'; 
 const TeamMembers = () =>{
     return (
       <div className="principal-container-teamMembers">
