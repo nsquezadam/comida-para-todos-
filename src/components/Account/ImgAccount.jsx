0416b97da58a148baec26f5c6ea304d1edf94203
danielablancom <blancodanielac@gmail.com>
2020-10-14 12:38:05 -0300

    adding pictures to Imgaccount component

--- src/components/Account/ImgAccount.jsx
+++ src/components/Account/ImgAccount.jsx
@@ -2,6 +2,9 @@ import React, { Fragment } from "react";
 import "../../assets/styles/components/ImgCompOurGoal.scss";
 import "../../assets/styles/components/ImgsComponent.scss";
 import techofraterno from "../../assets/static/Pics/techofraterno.jpg";
+import image2 from "../../assets/static/Pics/image2.jpeg";
+import image3 from "../../assets/static/Pics/image3.jpg";
+import image4 from "../../assets/static/Pics/image4.jpeg";
 import rendicion from "../../assets/static/images/rendicion.png";
 import LinksAccount from "./LinksAccount";
 
@@ -53,33 +56,21 @@ const ImgAccount = () => {
             <p className="name-imgs">Comida Para Todos Memoria</p>
             <p className="office1">2021 </p>
           </div>
-          <img
-            className="imgsComponents"
-            src={techofraterno}
-            alt={"techofraterno"}
-          ></img>
+          <img className="imgsComponents" src={image2} alt={"image2"}></img>
         </div>
         <div className="container-imgs">
           <div className="info-imgs">
             <p className="name-imgs">Comida Para Todos Memoria</p>
             <p className="office1">2022</p>
           </div>
-          <img
-            className="imgsComponents"
-            src={techofraterno}
-            alt={"techofraterno"}
-          ></img>
+          <img className="imgsComponents" src={image3} alt={"image3"}></img>
         </div>
         <div className="container-imgs">
           <div className="info-imgs">
             <p className="name-imgs">Comida Para Todos Memoria</p>
             <p className="office1">2023</p>
           </div>
-          <img
-            className="imgsComponents"
-            src={techofraterno}
-            alt={"techofraterno"}
-          ></img>
+          <img className="imgsComponents" src={image4} alt={"image4"}></img>
         </div>
       </div>
     </Fragment>
